f467746c18def740f72102fdeb7aaf2fdc310cc0
JeremyVe <jepschx@gmail.com>
2017-02-21 11:12:04 +0800

    setting redis for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,4 +83,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.middleware.use GameActionCable
+  config.web_socket_server_url = "wss://young-refuge-98238.herokuapp.com/" 
 end
--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,3 +1,3 @@
-$redis = Redis::Namespace.new('gof', :redis => Redis.new(:url => "redis://h:pdcf2ea033a7c3d850a1e6c049ea0c26cb0f6510d34c9eb2d907b78ef4c37ad5d@ec2-34-198-54-21.compute-1.amazonaws.com:25159"))
+$redis = Redis::Namespace.new('gof', :redis => Redis.new
 
 $redis.set('cells', {} )
